ac1ee87519b2756ba155c6f3721c2882a3e607f6
Manuel <Ramirez>
2020-08-21 11:58:17 -0400

    mern plantilla ready

--- roofingapp/server.js
+++ roofingapp/server.js
@@ -6,56 +6,56 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080; 
 
-const MONGODB_URI = 'mongodb+srv://manuelr14:leaddatabase@cluster0-h1cps.mongodb.net/<dbname>?retryWrites=true&w=majority'
-mongoose.connect (MONGODB_URI || 'mongodb://localhost/roofing_leads', {
+const routes = require('./routes/api');
+
+// go to mongodb.atlas to create cluster, hit new cluster, give a name, cluster will create in 10 min
+// database access, create user and password, give read and write access
+// network access allow access from anywhere.
+// go  back to cluster click conect and connect your app, copy the link, change password and db name 
+//uncomment next line if i want to use the cluster 
+// const MONGODB_URI = 'mongodb+srv://manuelr14:leaddatabase@cluster0-h1cps.mongodb.net/roofing_leads?retryWrites=true&w=majority'
+
+//uncoment next line if i want to order to connect to the mongo cluster if not possible conncet to the local one 
+//mongoose.connect (MONGODB_URI || 'mongodb://localhost/roofing_leads', {
+    mongoose.connect ('mongodb://localhost/roofing_leads', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 
 });
 
-// leaddatabase
+// listener, will tell me if mongoose is connected
 mongoose.connection.on('connected',() => {
     console.log('Mongoose is connected!!')
 });
 
-const Schema = mongoose.Schema;
-const CustomerSchema = new Schema({
-    name: String,
-    lastname: String,
-    phone: Number,
-    address: String, 
-    date:{
-        type: String,
-        default: Date.now()
-    }
-});
 
-const Customer = mongoose.model('Customer',CustomerSchema);
 
-const data = {
-    name: 'Jessika',
-    lastname: 'Ramirez',
-    phone: 4045555555,
-    address: '847 parc river blvd'
-};
 
-const newCustomer = new Customer(data);
-newCustomer.save((error) => {
-    if (error) {
-        console.log( 'something happened');
-    }else {
-        console.log( 'data saved!');
-    }
-});
+// creando data cualquiera
+// const data = {
+//     name: 'Jessika',
+//     lastname: 'Ramirez',
+//     phone: 4045555555,
+//     street: '847 parc river blvd',
+//     city: 'lawrenceville',
+//     zip: 30046
+// };
+
+// saving data to the mongo database usando .save y tiene un call back que me dice 
+//si salvo la data o no
+
+// const newCustomer = new Customer(data);
+// newCustomer.save((error) => {
+//     if (error) {
+//         console.log( 'something happened');
+//     }else {
+//         console.log( 'data saved!');
+//     }
+// });
 
 app.use(morgan('tiny')); 
+app.use('/', routes);
 
-app.get('/api/user',(req,res)=>{
-    const data = {
-        username: 'manuelr14',
-        age: 32
-    };
-    res.json(data);
-});
+
 
 app.listen(PORT, console.log(`Server is starting in ${PORT}`))
